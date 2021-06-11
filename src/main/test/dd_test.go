464483cb701ab491a4803a3771a4cb72f41de3b4
xiaoqian.deng <xiaoqian.deng@klook.com>
2021-06-11 19:39:03 +0800

    修复 测试注入

--- src/main/test/dd_test.go
+++ src/main/test/dd_test.go
@@ -1,9 +1,14 @@
 package test
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 	"testing"
-	"unsafe"
+	"time"
 )
 
 type UserHandler struct {
@@ -41,27 +46,157 @@ var GetName1 = func(defaultValue string) (string, error) {
 
 func TestPtrName(t *testing.T) {
 
-	u := &UserHandler{Id: 1234, Name: "xxxxxx"}
-	var m *UserHandler = u
-	fmt.Printf("%p %x \n", u, m)
-
-	//TODO 通过f获取对应的值
-	f := unsafe.Pointer(uintptr(unsafe.Pointer(u)) + unsafe.Offsetof(u.Id))
-	fmt.Printf("%p %x %x %d \n", f, f, &u.Id, *(*int)(f))
+	//u := &UserHandler{Id: 1234, Name: "xxxxxx"}
+	//var m *UserHandler = u
+	//fmt.Printf("%p %x \n", u, m)
+	//
+	////TODO 通过f获取对应的值
+	//f := unsafe.Pointer(uintptr(unsafe.Pointer(u)) + unsafe.Offsetof(u.Id))
+	//fmt.Printf("%p %x %x %d \n", f, f, &u.Id, *(*int)(f))
 
 	//rt := reflect.ValueOf(u).MethodByName("GetName")
 	//fmt.Printf("%d %s \n",uintptr(rt.Pointer()),reflect.ValueOf(rt.Interface()).Kind())
 	//rt.SetPointer(unsafe.Pointer(&GetName1))
 	//rt.Set(reflect.ValueOf(&GetName1))
 
-	var mm []string = nil
+	//var mm []string = nil
+	//
+	//for _, m1 := range mm {
+	//	fmt.Printf(m1)
+	//}
+	//
+	//var m2 []interface{}
+	//m2 = append(m2, map[string]string{"a": "b"})
+	//m2 = append(m2, map[string]int{"a": 1})
+	//fmt.Println(m2)
 
-	for _, m1 := range mm {
-		fmt.Printf(m1)
+	//var path string = "/Users/klook/log/access.log"
+	//var tag string = "tt000"
+	//var tag1 string = "tt111"
+	//var tag2 string = "tt222"
+	//go testFile(path,tag)
+	//go testFile(path,tag1)
+	//go testFile(path,tag2)
+	//var m chan int = make(chan int)
+	//<- m
+	f := "%date %date{2006-01-02} %date{2006-01-02T15:04:05Z07:00} %-5thread %-5level %logger %logger{5} %-30logger{5} %thread %-5line %file %msg %n "
+	//reg := regexp.MustCompile(`(?m)(^\s+|\s+$)`)
+	//reg := regexp.MustCompile("%(\\{[^\\}]+\\})?(date|thread|level|logger|line|file|msg|n)(\\{[^\\}]+\\})?")
+	//last := reg.ReplaceAllStringFunc(f,func(row string) string{
+	//	fmt.Println(row)
+	//	return ""
+	//})
+	//fmt.Println("--->",last)
+	// date|thread|level|line|file|msg|n|logger
+	var date1 = regexp.MustCompile("%date(\\{[^\\}]+\\})?")
+	f = date1.ReplaceAllStringFunc(f, func(row string) string {
+		p := strings.Index(row, "{")
+		dateFormat := "2006-01-02 15:04:05"
+		if p >= 0 {
+			p1 := strings.Index(row, "}")
+			dateFormat = row[p+1 : p1]
+		}
+		return fmt.Sprintf(`{{logDate "%s"}}`, dateFormat)
+	})
+	var thread1 = regexp.MustCompile("%(\\-\\d+)?thread")
+	f = thread1.ReplaceAllStringFunc(f, func(row string) string {
+		p := strings.Index(row, "-")
+		maxSize := 0
+		if p >= 0 {
+			p1 := strings.Index(row, "thread")
+			maxSizeStr := row[p+1 : p1]
+			maxSize, _ = strconv.Atoi(maxSizeStr)
+		}
+		return fmt.Sprintf(`{{logThread %d}}`, maxSize)
+	})
+	var level1 = regexp.MustCompile("%(\\-\\d+)?level")
+	f = level1.ReplaceAllStringFunc(f, func(row string) string {
+		p := strings.Index(row, "-")
+		maxSize := 0
+		if p >= 0 {
+			p1 := strings.Index(row, "level")
+			maxSizeStr := row[p+1 : p1]
+			maxSize, _ = strconv.Atoi(maxSizeStr)
+		}
+		return fmt.Sprintf(`{{logLevel %d}}`, maxSize)
+	})
+
+	//line|file|msg|n|logger
+	var line1 = regexp.MustCompile("%(\\-\\d+)?line")
+	f = line1.ReplaceAllStringFunc(f, func(row string) string {
+		p := strings.Index(row, "-")
+		maxSize := 0
+		if p >= 0 {
+			p1 := strings.Index(row, "line")
+			maxSizeStr := row[p+1 : p1]
+			maxSize, _ = strconv.Atoi(maxSizeStr)
+		}
+		return fmt.Sprintf(`{{logLine %d}}`, maxSize)
+	})
+
+	var file1 = regexp.MustCompile("%(\\-\\d+)?file")
+	f = file1.ReplaceAllStringFunc(f, func(row string) string {
+		p := strings.Index(row, "-")
+		maxSize := 0
+		if p >= 0 {
+			p1 := strings.Index(row, "file")
+			maxSizeStr := row[p+1 : p1]
+			maxSize, _ = strconv.Atoi(maxSizeStr)
+		}
+		return fmt.Sprintf(`{{logFile %d}}`, maxSize)
+	})
+	//msg|n|logger
+	var msg1 = regexp.MustCompile("%(\\-\\d+)?msg")
+	f = msg1.ReplaceAllStringFunc(f, func(row string) string {
+		return fmt.Sprintf(`{{logMsg %d}}`, 0)
+	})
+	var br = regexp.MustCompile("%(\\-\\d+)?n")
+	f = br.ReplaceAllStringFunc(f, func(row string) string {
+		//return fmt.Sprintf(`{{logBr %d}}`,0)
+		return `\n`
+	})
+	//%logger{n}
+	var logger1 = regexp.MustCompile("%(\\-\\d+)?logger(\\{[^\\}]+\\})?")
+	f = logger1.ReplaceAllStringFunc(f, func(row string) string {
+		//return fmt.Sprintf(`{{logBr %d}}`,0)
+		p := strings.Index(row, "-")
+		maxSize := 0
+		if p >= 0 {
+			p1 := strings.Index(row, "logger")
+			maxSizeStr := row[p+1 : p1]
+			maxSize, _ = strconv.Atoi(maxSizeStr)
+		}
+
+		p1 := strings.Index(row, "{")
+		clazzSize := -1
+		if p1 >= 0 {
+			p2 := strings.Index(row, "}")
+			clazzSizeStr := row[p1+1 : p2]
+			clazzSize, _ = strconv.Atoi(clazzSizeStr)
+		}
+		return fmt.Sprintf(`{{logLogger %d %d}}`, maxSize, clazzSize)
+	})
+
+	fmt.Println("--->", f)
+}
+
+func testFile(path string, tag string) {
+	f1, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
 	}
-
-	var m2 []interface{}
-	m2 = append(m2, map[string]string{"a": "b"})
-	m2 = append(m2, map[string]int{"a": 1})
-	fmt.Println(m2)
+	defer func() {
+		f1.Close()
+	}()
+	b1 := bufio.NewWriter(f1)
+	i := 0
+	for {
+		b1.WriteString(fmt.Sprintf("begin 测试生生世世生生世世 %s %d\n测试生生世世生生世世 %s %d end \n", tag, i, tag, i))
+		i = i + 1
+		if i/10 == 0 {
+			b1.Flush()
+			time.Sleep(2 * time.Second)
+		}
+	}
+	f1.Sync()
 }
