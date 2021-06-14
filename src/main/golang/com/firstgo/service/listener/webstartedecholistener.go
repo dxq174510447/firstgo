f984a15b2680410f44a1635ecfb98c6e8ffc872f
dengxiaoqian <dxq174510447@163.com>
2021-06-14 22:01:37 +0800

    v1

---
+++ src/main/golang/com/firstgo/service/listener/webstartedecholistener.go
@@ -0,0 +1,40 @@
+package listener
+
+import (
+	"github.com/dxq174510447/goframe/lib/frame/application"
+	"github.com/dxq174510447/goframe/lib/frame/context"
+	"github.com/dxq174510447/goframe/lib/frame/event"
+	"github.com/dxq174510447/goframe/lib/frame/http"
+	"github.com/dxq174510447/goframe/lib/frame/proxy/proxyclass"
+	"github.com/dxq174510447/goframe/lib/frame/util"
+	"reflect"
+)
+
+type WebStartedEchoListener struct {
+	Logger application.AppLoger `FrameAutowired:""`
+}
+
+func (w *WebStartedEchoListener) OnEvent(local *context.LocalStack, event event.FrameEventer) error {
+	f := util.ClassUtil.GetClassNameByType(reflect.TypeOf(event).Elem())
+	w.Logger.Info(local, "启动 %s", f)
+	return nil
+}
+
+func (w *WebStartedEchoListener) WatchEvent() event.FrameEventer {
+	e := &http.WebServletStartedEvent{}
+	return event.FrameEventer(e)
+}
+
+func (w *WebStartedEchoListener) Order() int {
+	return 0
+}
+
+func (w *WebStartedEchoListener) ProxyTarget() *proxyclass.ProxyClass {
+	return nil
+}
+
+var webStartedEchoListener WebStartedEchoListener = WebStartedEchoListener{}
+
+func init() {
+	application.AddProxyInstance("", proxyclass.ProxyTarger(&webStartedEchoListener))
+}
--- src/main/golang/com/firstgo/service/module.go
+++ src/main/golang/com/firstgo/service/module.go
@@ -3,6 +3,7 @@ package service
 import (
 	_ "firstgo/src/main/golang/com/firstgo/service/httpfilter"
 	_ "firstgo/src/main/golang/com/firstgo/service/impl"
+	_ "firstgo/src/main/golang/com/firstgo/service/listener"
 )
 
 func init() {
